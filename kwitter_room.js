5884c7bb1248d805abcb46fd09252843345568e2
Abhishekar0308 <88946903+Abhishekar0308@users.noreply.github.com>
2021-12-30 17:56:31 -0600

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBg1pT15qGTnZ8RdqjZLDbQsoPxhfZKmUU",
+    authDomain: "kwitter-project-53cfc.firebaseapp.com",
+    projectId: "kwitter-project-53cfc",
+    storageBucket: "kwitter-project-53cfc.appspot.com",
+    messagingSenderId: "594981871785",
+    appId: "1:594981871785:web:f65118dd27de0f23df49b8"
+  };
+  
+  const app = initializeApp(firebaseConfig);
